e2c26a89c07ef7a4c0732a203bd88d9773882b2c
jsugarman <joel.sugarman@digital.justice.gov.uk>
2015-05-21 12:02:35 +0100

    provide field adjacent error messages for signup

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,10 @@ module MyShop
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+
+    config.action_view.field_error_proc = proc do |html_tag, instance_tag|
+        "<span class=\"field_with_errors\">#{html_tag}</span>".html_safe
+    end
   end
 end
